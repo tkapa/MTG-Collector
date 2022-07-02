ef667d4888ca28353b2d2c7eb20e8221598e3ab5
tkapa <tylahkapa@gmail.com>
2022-07-02 23:36:33 +1000

    Add project files.

---
+++ MTG-Collector/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MTG_Collector;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MTG-Collector/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MTG_Collector;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
